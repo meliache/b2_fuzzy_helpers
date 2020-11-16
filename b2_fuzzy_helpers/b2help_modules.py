0cac63eddebe2a4e99a19d6e6c708a6007247aba
Michael Eliachevitch <meliache@uni-bonn.de>
2020-11-16 22:51:55 +0100

    Get module help via from `print_params` instead of subprocess call

--- b2_fuzzy_helpers/b2help_modules.py
+++ b2_fuzzy_helpers/b2help_modules.py
@@ -2,7 +2,6 @@
 """
 Helper script to quickly find module descriptions in basf2
 """
-from subprocess import run
 from iterfzf import iterfzf
 import basf2
 
@@ -10,8 +9,7 @@ import basf2
 def fuzzy_find_and_describe_module() -> None:
     """Use fzf to find a basf2 module and print module help"""
     module_name = iterfzf(module for module in basf2.list_available_modules().keys())
-    cmd = ["basf2", "-m", module_name]
-    run(cmd, check=True)
+    basf2.utils.print_params(basf2.register_module(module_name))
 
 
 if __name__ == '__main__':
