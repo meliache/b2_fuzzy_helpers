b79b7e92e0e013df638deeda2c8f959601ed3005
Michael Eliachevitch <meliache@uni-bonn.de>
2020-11-16 20:10:39 +0100

    Simplify code by using vm.getNames()

--- b2_fuzzy_helpers/b2help_variables.py
+++ b2_fuzzy_helpers/b2help_variables.py
@@ -8,7 +8,7 @@ from variables import variables as vm
 
 
 def fuzzy_find_and_describe_variable() -> None:
-    variable_name = iterfzf(var.name for var in vm.getVariables())
+    variable_name = iterfzf(vm.getNames())
     description = vm.getVariable(variable_name).description
     print(f"Variable: {variable_name}\nDescription: {description}")
 
