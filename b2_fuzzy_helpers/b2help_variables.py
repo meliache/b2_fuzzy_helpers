b3171ed2f2021d8f13da252039d895c2fd1d2760
Michael Eliachevitch <m.eliachevitch@posteo.de>
2022-04-22 16:08:39 +0200

    Fix

--- b2_fuzzy_helpers/b2help_variables.py
+++ b2_fuzzy_helpers/b2help_variables.py
@@ -3,7 +3,7 @@
 Helper script to quickly find variable descriptions in basf2
 """
 
-from typing import List
+from typing import Dict
 
 from iterfzf import iterfzf
 from joblib import Memory
@@ -16,30 +16,32 @@ from b2_fuzzy_helpers.utils import get_basf2_git_hash
 
 # make cached version of basf2 get_variables, i.e. save variable list for a specific release
 @memory.cache
-def get_variables(
+def get_variable_info_dict(
     _basf2_release: str,
-) -> List[str]:  # release parameter only used as cache key
+) -> Dict[str, Dict[str, str]]:  # release parameter only used as cache key
     print("Loading variable manager…", end="\r")
     from variables import variables as vm
 
-    return [v for v in vm.getVariables()]
-
-
-def fuzzy_find_and_describe_variable() -> None:
-    variables = get_variables(get_basf2_git_hash())
-    variable_info_dict = {
+    variables = vm.getVariables()
+    return {
         var.name: {
             "group": var.group,
             "description": var.description,
         }
         for var in variables
     }
+
+
+def fuzzy_find_and_describe_variable() -> None:
+
+    variable_info_dict = get_variable_info_dict(get_basf2_git_hash())
     var_name = iterfzf(variable_info_dict.keys())
     var_info = variable_info_dict[var_name]
     print(
         f"Variable: {var_name}\nGroup: {var_info['group']}\nDescription: {var_info['description']}",
         flush=True,
     )
+
 
 if __name__ == "__main__":
     fuzzy_find_and_describe_variable()
